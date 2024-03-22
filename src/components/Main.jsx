e7745e4acd89e7c4087f046b0a32ad6052345aad
itzkunal01 <kunal.sharmag12@gmail.com>
2024-03-23 01:17:10 +0530

    updating code

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Main = ({ name }) => {
   return (
     <>
-      <div className="text-center p-5 text-danger">
-        This is my {name} section.
+      <div className="text-center p-5 text-danger text-uppercase">
+        <h2>This is my {name} section.</h2>
       </div>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = ({ updateMyFunction }) => {
   const data = ["home", "hero", "spider-man", "batman"];
   return (
     <>
-      <div className="d-flex align-items-center justify-content-center gap-2">
+      <div className="d-flex align-items-center justify-content-center gap-2 py-5">
         {data.map((obj, index) => (
           <button key={index} onClick={() => updateMyFunction(obj)}>
             {obj}
