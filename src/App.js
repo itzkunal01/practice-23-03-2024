bcb5e8146e551923548f7114b48f7a25cd8680ef
itzkunal01 <kunal.sharmag12@gmail.com>
2024-03-23 02:16:34 +0530

    updating useEffect code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import A from "./components/Navbar";
 import B from "./components/Main";
 
 const App = () => {
-  const [name, setName] = React.useState("nothing");
+  // 1.
+  const [name, setName] = useState("nothing");
   const updateMyFunction = (e) => {
     setName(e);
     console.log(e);
   };
 
+  // 2.
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const loader = setInterval(() => {
+      if (count < 10) {
+        setCount((count) => count + 1);
+      }
+    }, 1000);
+
+    return () => {
+      clearInterval(loader);
+    };
+  }, [count]);
+
   return (
     <>
-      <A updateMyFunction={updateMyFunction} />
-      <B name={name} />
+      <div className="position-relative">
+        <div className="position-absolute top-50 start-50 p-5 bg-dark text-white rounded">
+          <h1>
+            <b>{count}</b>
+          </h1>
+        </div>
+
+        <A updateMyFunction={updateMyFunction} />
+        <B name={name} />
+      </div>
     </>
   );
 };
